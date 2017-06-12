0d3b5eaf41ca9322737a80455f4915fc47ea7433
scan77 <scan77@ESG102PC077.home.e-kmall.com>
2017-06-12 22:43:10 +0900

    variable

---
+++ Variable/variable/variable.py
@@ -0,0 +1,12 @@
+'''
+Created on 2017. 6. 12.
+
+@author: scan77
+'''
+identity = '지구인'
+number_of_legs = 4
+
+if __name__ == '__main__':
+    print('안녕! 나는 지구인 이야. 나는 다리가 4 개 있어.')
+    print('안녕! 나는', '지구인', '이야. 나는 다리가', 4, '개 있어.')
+    print('안녕! 나는', identity, '이야. 나는 다리가', number_of_legs, '개 있어.')
